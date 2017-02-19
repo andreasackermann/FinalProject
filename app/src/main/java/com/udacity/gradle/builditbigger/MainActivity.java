819db8cd39c78554e0a65e61c027cfb6364fec3f
Andreas Ackermann <mail@andreasackermann.de>
2017-02-19 15:58:48 +0100

    add android library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.JokeSupplier;
+import com.example.jokeactivityandlib.TellJokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
