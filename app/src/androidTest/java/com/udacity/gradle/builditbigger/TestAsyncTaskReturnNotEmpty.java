c08c0ed7ed73eeb81f26875089520ca618f93eef
Andreas Ackermann <mail@andreasackermann.de>
2017-02-21 17:55:22 +0100

    add backend

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TestAsyncTaskReturnNotEmpty.java
@@ -0,0 +1,18 @@
+package com.udacity.gradle.builditbigger;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+/**
+ * Created by Andreas on 19.02.2017.
+ */
+
+public class TestAsyncTaskReturnNotEmpty {
+
+    @Test
+    public void verifyAsyncTaskReturnNotEmpty() {
+        Log.wtf("grmblfpfmf","Blubb");
+    }
+
+}
